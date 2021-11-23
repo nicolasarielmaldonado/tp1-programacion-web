f281e20fcdfb04fbde2c776b9be305f49d6f1663
Facundo Damián Fagnano <facundofagnano@gmail.com>
2021-11-22 21:51:14 -0300

    js en contacto

--- js/validacion-formulario.js
+++ js/validacion-formulario.js
@@ -1,102 +1,147 @@
-
-function validar(){
+function validar() {
     var regexemail = /^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/;
-    var mensaje =""; 
-    var error =0;                
+    var mensaje = "";
+    var error = 0;
     var regextelefono = /[0-9]+$/;
     console.log("Llegó a la función");
     reset();
-    if ($("#nombre").val()==""){
-        
-        mensaje+= "<p>El campo nombre es obligatorio </p>";
-        error++;
-        $("#nombre").addClass('error-inscripcion');
+    if ($("#nombre").val() == "") {
+      mensaje += "<p>El campo nombre es obligatorio </p>";
+      error++;
+      $("#nombre").addClass("error-inscripcion");
     }
-    if($("#apellido").val()==""){
-        mensaje+= "<p>El campo apellido es obligatorio </p>";
-        error++;
-        $("#apellido").addClass('error-inscripcion');
+    if ($("#apellido").val() == "") {
+      mensaje += "<p>El campo apellido es obligatorio </p>";
+      error++;
+      $("#apellido").addClass("error-inscripcion");
     }
-    if (!($('#telefono').val().match(regextelefono))) { 
-        mensaje+= "<p>Debe agregar un número de telefóno</p>";  
-        error++;
+    if (!$("#telefono").val().match(regextelefono)) {
+      mensaje += "<p>Debe agregar un número de telefóno</p>";
+      error++;
     }
-    if(!$("#email").val().match(regexemail)){
-        mensaje+= "<p>Debe ser un email valido</p>";
-        error++;
-        $("#email").addClass('error-inscripcion');
+    if (!$("#email").val().match(regexemail)) {
+      mensaje += "<p>Debe ser un email valido</p>";
+      error++;
+      $("#email").addClass("error-inscripcion");
     }
-    if ($("#provincia option:selected").val()==0){
-        mensaje+= "<p>Debe seleccionar una provincia</p>";
-        error++;
-        $("#provincia").addClass('error-inscripcion');
+    if ($("#provincias option:selected").val() == 0) {
+      mensaje += "<p>Debe seleccionar una provincia</p>";
+      error++;
+      $("#provincias").addClass("error-inscripcion");
     }
-    if ($("#localidad option:selected").val()==0){
-        mensaje+= "<p>Debe seleccionar una localidad</p>";
-        error++;
-        $("#localidad").addClass('error-inscripcion');
+  
+    if (
+      $("#provincias option:selected").val() != 0 &&
+      $("#municipios option:selected").val() == 0
+    ) {
+      mensaje += "<p>Debe seleccionar un municipio</p>";
+      error++;
+      $("#municipios").addClass("error-inscripcion");
     }
-    
-    
-    if (error>0){
-        console.log(error);
-        $("#mensaje-inscripcion").append(mensaje); 
-        return false;
+    if ($("#comments").val() == "") {
+      mensaje += "<p>Debe ingresar un comentario</p>";
+      error++;
+      $("#comments").addClass("error-inscripcion");
     }
-   
-    else{
-        return true;
+    if (error > 0) {
+      $("#mensaje-inscripcion").append(mensaje);
+      return false;
+    } else {
+      return true;
     }
-
-}
-function reset(){
-    $("#nombre").removeClass('error-inscripcion');
-    $("#apellido").removeClass('error-inscripcion');
-    $("#provincia").removeClass('error-inscripcion');
-    $("#email").removeClass('error-inscripcion');
-    $("#localidad").removeClass('error-inscripcion');
-    $("#telefono").removeClass('error-inscripcion');
+  }
+  function reset() {
+    $("#nombre").removeClass("error-inscripcion");
+    $("#apellido").removeClass("error-inscripcion");
+    $("#provincias").removeClass("error-inscripcion");
+    $("#email").removeClass("error-inscripcion");
+    $("#municipios").removeClass("error-inscripcion");
+    $("#telefono").removeClass("error-inscripcion");
+    $("#comments").removeClass("error-inscripcion");
     $("#mensaje-inscripcion").empty();
-}
-
-$(document).ready(function() {
-    
-    $("#form").submit(function() {
-        return validar();
-    });
-    $("#nombre").keyup(function() {
-        validar();
-    });
-    $("#apellido").keyup(function() {
-        validar();
-    });
-    $('#telefono').keyup(function() {
-        validar("#telefono");
-    });
-    $("#email").keyup(function() {
-        validar("#email");
-    });
-    $("#provincia").change(function() {
-        validar();
-    });
-   /* $("#localidad").change(function() {
-        validar();
-    });*/
-       
-});
-
-const options = {
+  }
+  
+  function obtenerItemLocalStorage() {
+    const establecimiento = localStorage.getItem("establecimiento");
+    const lenguaje = localStorage.getItem("lenguaje");
+    const horario = localStorage.getItem("horario");
+    const precio = localStorage.getItem("precio");
+  
+    return (datosDeInscripcion = { establecimiento, lenguaje, horario, precio });
+  }
+  
+  function renderizarDatosEnSeccion() {
+    const datos = obtenerItemLocalStorage();
+  
+    $("#datos-sede-horario").html(
+      `Inscripción al curso de idioma ${datos.lenguaje} en la sede ${datos.establecimiento} en el turno ${datos.horario}.`
+    );
+    $("#datos-precio").html(`Precio: $${datos.precio}`);
+  }
+  
+  const options = {
     method: "GET",
     headers: {
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/json",
+    },
   };
-
-  fetch("https://apis.datos.gob.ar/georef/api/localidades?formato=json&max=5000", options)
-    .then(response => response.json())
-    .then(data => {
-      let localidades = data.results;
-      return localidades.map (localidades => {
-
-      })
+  
+  function obtenerProvincias() {
+    fetch(
+      "https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre",
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        for (provincia of data.provincias) {
+          const prov = document.createElement("option");
+          prov.value = parseInt(provincia.id) + 1;
+          prov.textContent = provincia.nombre;
+          provincias.appendChild(prov);
+        }
+      });
+  }
+  
+  $(document).ready(function () {
+    renderizarDatosEnSeccion();
+    obtenerProvincias();
+  
+    $("#form").submit(function () {
+      return validar();
     });
+    $("#nombre").keyup(function () {
+      validar();
+    });
+    $("#apellido").keyup(function () {
+      validar();
+    });
+    $("#telefono").keyup(function () {
+      validar("#telefono");
+    });
+    $("#email").keyup(function () {
+      validar("#email");
+    });
+    $("#comments").keyup(function () {
+      validar();
+    });
+    $("#provincias").change(function (e) {
+      validar();
+      fetch(
+        `https://apis.datos.gob.ar/georef/api/municipios?provincia=${
+          $("#provincias option:selected").val() - 1
+        }&max=1000`,
+        options
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          for (municipio of data.municipios) {
+            const muni = document.createElement("option");
+            muni.value = parseInt(municipio.id) + 1;
+            muni.textContent = municipio.nombre;
+            municipios.appendChild(muni);
+          }
+        });
+        
+    });
+  });
+  
